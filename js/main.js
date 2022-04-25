03fa4217808848e287bdbba56f2dc4fd49fdf4d6
SamedovRasul <samedovrasul7@gmail.com>
2022-04-25 19:33:34 +0400

    test

--- js/main.js
+++ js/main.js
@@ -106,7 +106,7 @@ const submit = document.getElementById('btn');
 let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 submit.addEventListener('click', function(){
 
-  // test
+  // tes
   const data ={
     name:Name.value,
     surname:surname.value,
