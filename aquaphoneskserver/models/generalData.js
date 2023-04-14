da21b3013b5c096144bc96cddbcaf818f2e6a4a7
unknown <xeyyamomarov2014@gmail.com>
2023-04-14 11:05:23 +0400

    ADD  AZIRRIGATION SITE CODE

--- aquaphoneskserver/models/generalData.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const generalData = mongoose.Schema({
-  temp:Array
-})
-
-const generalDataSchema = mongoose.model("generalDataSchema", generalData)
-
-export default generalDataSchema
--- aquaphoneskserver/router/index.js
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import { tempraturePost,tempratureGet } from '../controller/temprature.js'
-
-const router = express.Router()
-
-router.post('/', tempraturePost);
-router.get('/', tempratureGet);
-
-export default router
